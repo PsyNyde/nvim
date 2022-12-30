0ea27b65cdce6ef5326a126b50e47d41748e38e8
SimplyVoid <wahidislamlinad@pm.me>
2022-12-31 00:18:33 +0600

    More Cleanup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,16 +1,25 @@
-return {{"tpope/vim-fugitive"}, {"tpope/vim-surround"}, {
-    "norcalli/nvim-colorizer.lua",
-    config = true
-}, {
-    "windwp/nvim-autopairs",
-    config = true
-}, {"nvim-tree/nvim-web-devicons"}, {
-    "folke/which-key.nvim",
-    lazy = false
-}, {"williamboman/mason-lspconfig.nvim"}, {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime",
-    config = function()
-        vim.g.startuptime_tries = 10
-    end
-}}
+return {
+	{"tpope/vim-fugitive"}, 
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = true
+	}, 
+	{
+		"windwp/nvim-autopairs",
+		config = true
+	}, 
+	{"nvim-tree/nvim-web-devicons"}, 
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		config = true
+	}, 
+	{"williamboman/mason-lspconfig.nvim"}, 
+	{
+		"dstein64/vim-startuptime",
+		cmd = "StartupTime",
+		config = function()
+			vim.g.startuptime_tries = 10
+		end
+	},
+}
